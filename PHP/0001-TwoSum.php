78b229a4305c61456192bf3f14c4d5d736a33d4f
TomLi <open222333@gmail.com>
2022-03-13 13:18:53 +0800

    20220313

---
+++ PHP/0001-TwoSum.php
@@ -0,0 +1,42 @@
+<?php
+class Solution
+{
+
+	/**
+	 * @param Integer[] $nums
+	 * @param Integer $target
+	 * @return Integer[]
+	 */
+	function twoSum(array $nums, int $target)
+	{
+		$map = [];
+		foreach ($nums as $num) {
+			$diff = $target - $num;
+			if (array_key_exists($diff, $nums)) {
+				$key_2 = array_search($diff, $map);
+				$key_1 = array_search($num, $map);
+				return [$key_1, $key_2];
+			}
+			$map[array_search($num, $nums)] = $num;
+		}
+	}
+}
+
+// A slightly optimized version of Approach 3 - One Pass Hash Table
+// 8ms, 99%tile speed, 8%tile memory
+class Solution1
+{
+	function twoSum(array $nums, int $target)
+	{
+		$map = [];
+		// Important to not have count($nums) in the `for` loop. Else it will get executed every iteration.
+		$size = count($nums);
+		for ($i = 0; $i < $size; $i++) {
+			$complement = $target - $nums[$i];
+			if (array_key_exists($complement, $map)) {
+				return [$i, $map[$complement]];
+			}
+			$map[$nums[$i]] = $i;
+		}
+	}
+}
